e28be449162cd560e691cd164d28caa03c1b9a81
driftshieldai <driftshieldai@gmail.com>
2026-02-21 14:15:51 +0000

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "The GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "image_tag" {
+  description = "The Docker image tag to deploy (passed from GitHub Actions)"
+  type        = string
+}
+
+variable "gar_repo_name" {
+  description = "Name of the Artifact Registry Repo"
+  type        = string
+  default     = "alzheimer-ui-docker"
+}
