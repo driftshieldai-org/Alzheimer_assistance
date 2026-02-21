f9b692183781b183e75edb357c0933016e30168f
driftshieldai <driftshieldai@gmail.com>
2026-02-21 14:33:18 +0000

    Create variables.tf

---
+++ terraform-deploy/variables.tf
@@ -0,0 +1,41 @@
+variable "project_id" {
+  description = "The GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "repo_name" {
+  description = "Name of the Artifact Registry Repo"
+  type        = string
+}
+
+variable "service_account_id" {
+  description = "The GCP service account used for application"
+  type        = string
+}
+
+variable "vpc_network" {
+  description = "Name of the VPC network"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Name of vpc subnet"
+  type        = string
+}
+
+variable "ui_image_name" {
+  description = "Name of image created for UI cloud run"
+  type        = string
+}
+
+variable "cloudrun_name" {
+  description = "Name of cloud run service created for UI"
+  type        = string
+}
+
